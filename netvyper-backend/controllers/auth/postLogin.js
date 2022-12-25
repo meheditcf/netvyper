365e213d33a3ca199921d760e52975e486383aad
Mehedi Zamadar <meheditcf@gmail.com>
2022-12-25 21:40:18 +0600

    create login controller

--- netvyper-backend/controllers/auth/postLogin.js
+++ netvyper-backend/controllers/auth/postLogin.js
@@ -1,5 +1,24 @@
+const User = require("../../models/user");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+
 const postLogin = async (req, res) => {
-  res.send("login route");
+  try {
+    const { password, mail } = req.body;
+    const user = await User.findOne({ mail: mail.toLowerCase() });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      const token = "JWT";
+      res.status(201).json({
+        mail: mail,
+        token: token,
+      });
+    } else {
+      return res.status(401).send("Invalid credentials");
+    }
+  } catch (err) {
+    return res.status(500).send(err);
+  }
 };
 
 module.exports = postLogin;
