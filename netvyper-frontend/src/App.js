1bca8e52c531c036a397305479b0483a2930b515
Mehedi Zamadar <meheditcf@gmail.com>
2022-12-26 22:46:42 +0600

    add tailwind css

--- netvyper-frontend/src/App.js
+++ netvyper-frontend/src/App.js
@@ -1,9 +1,21 @@
+import React from "react";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import LoginComponent from "./components/auth/Login";
+import RegisterComponent from "./components/auth/Register";
+import DashboardComponent from "./components/dashboard/Dashboard";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hi</h1>
-    </div>
+    <>
+    <Router>
+      <Routes>
+        <Route exact path="/login" element={<LoginComponent></LoginComponent>}/>
+        <Route exact path="/register" element={<RegisterComponent></RegisterComponent>}/>
+        <Route exact path="/dashboard" element={<DashboardComponent></DashboardComponent>}/>
+        <Route exact path="/" element={<DashboardComponent></DashboardComponent>}/>
+      </Routes>
+    </Router>
+    </>
   );
 }
 
