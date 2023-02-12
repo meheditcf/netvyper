7cec37b2666eb0bd7a89a9031cfa7899a125b0c4
Mehedi Zamadar <meheditcf@gmail.com>
2023-02-12 22:22:04 +0600

    implement login register thunk functions

--- netvyper-frontend/src/components/auth/Login.js
+++ netvyper-frontend/src/components/auth/Login.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function LoginComponent() {
+
   const navigate = useNavigate();
 
   const handlePushToRegisterPage = () => {
--- netvyper-frontend/src/store/authSlice.js
+++ netvyper-frontend/src/store/authSlice.js
@@ -1,15 +1,49 @@
-import {createSlice} from "@reduxjs/toolkit";
+import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
+import {login, register} from "../api";
 
 const initialState = {
-    value: 0
+    userDetails: [],
+    loading: false,
 }
 
 export const authSlice = createSlice({
     name: 'auth',
     initialState,
-    reducers: {}
+    reducers: {
+        setUserDetails: (state, action) => {
+            state.userDetails = action.payload;
+        }
+    }
 })
 
+export const loginUser = (userDetails, history) => {
+    return async (dispatch) => {
+        const res = await login(userDetails);
+        if (res.error) {
+            console.log('user login error')
+        } else {
+            const {userDetails} = res.data;
+            localStorage.setItem('user', JSON.stringify(userDetails))
+            dispatch(setUserDetails(userDetails))
+            history.push('/dashboard')
+        }
+    }
+}
 
-export const {} = authSlice.actions
+export const registerUser = (userDetails, history) => {
+    return async (dispatch) => {
+        const res = await register(userDetails);
+        if (res.error) {
+            console.log('user register error')
+        } else {
+            const {userDetails} = res?.data;
+            localStorage.setItem('user', JSON.stringify(userDetails))
+            dispatch(setUserDetails(userDetails))
+            history.push('/dashboard')
+        }
+    }
+}
+
+
+export const {setUserDetails} = authSlice.actions
 export default authSlice.reducer
